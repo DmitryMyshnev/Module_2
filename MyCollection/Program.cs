5582bec7e69054d0ea7235dffdc5c5c52798ac83
Дмитрий Мишнев <dmitrymishnev@gmail.com>
2020-01-13 21:14:33 +0200

    add new task

---
+++ MyCollection/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ranList = new SimpleNumbers(1, 10);
+
+            foreach (var item in ranList.simpleNumbers)
+            {
+                if (ranList.SimpleCheck())
+                    Console.WriteLine(item);
+                ranList.MoveNext();
+            }
+        }
+    }
+}
---
+++ MyCollection/SimpleNumbers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCollection
+{
+    class SimpleNumbers : IEnumerator
+    {
+        public List<int> simpleNumbers = new List<int>(10);
+        int index = 0;
+        public SimpleNumbers(int min, int max)
+        {
+            var rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                simpleNumbers.Add(rnd.Next(min, max));
+
+            }
+
+        }
+
+        public int Current
+        {
+            get
+            {
+                return simpleNumbers[index];
+            }
+        }
+        object IEnumerator.Current => throw new NotImplementedException();
+
+
+        public bool MoveNext()
+        {
+            if (index <= 10)
+            {
+                index++;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public void Reset()
+        {
+            index = 0;
+        }
+        public bool SimpleCheck()
+        {
+            bool check = true;
+            for (int i = 2; i < Current; i++)
+            {
+                if ((Current % i) != 0)
+                    check = true;
+                else
+                    return false;
+
+            }
+            return check;
+        }
+    }
+}
+
